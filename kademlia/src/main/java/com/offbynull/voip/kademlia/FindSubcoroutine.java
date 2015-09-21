e9c6b2b471aaaa4ab41b71d4fb0ec0769c6b74f7
offbynull <offbynull@gmail.com>
2015-09-20 23:46:18 -0700

    More work kademlia

--- kademlia/src/main/java/com/offbynull/voip/kademlia/FindSubcoroutine.java
+++ kademlia/src/main/java/com/offbynull/voip/kademlia/FindSubcoroutine.java
@@ -6,14 +6,21 @@ import com.offbynull.peernetic.core.actor.helpers.AddressTransformer;
 import com.offbynull.peernetic.core.actor.helpers.IdGenerator;
 import com.offbynull.peernetic.core.actor.helpers.MultiRequestSubcoroutine;
 import com.offbynull.peernetic.core.actor.helpers.MultiRequestSubcoroutine.Response;
+import com.offbynull.peernetic.core.actor.helpers.RequestSubcoroutine;
 import com.offbynull.peernetic.core.actor.helpers.Subcoroutine;
+import com.offbynull.peernetic.core.actor.helpers.SubcoroutineRouter;
+import com.offbynull.peernetic.core.actor.helpers.SubcoroutineRouter.AddBehaviour;
+import com.offbynull.peernetic.core.actor.helpers.SubcoroutineRouter.Controller;
+import com.offbynull.peernetic.core.actor.helpers.SubcoroutineRouter.ForwardResult;
 import static com.offbynull.peernetic.core.gateways.log.LogMessage.info;
 import com.offbynull.peernetic.core.shuttle.Address;
+import com.offbynull.voip.kademlia.externalmessages.FindRequest;
 import com.offbynull.voip.kademlia.externalmessages.FindResponse;
 import com.offbynull.voip.kademlia.model.Id;
 import com.offbynull.voip.kademlia.model.IdClosenessComparator;
 import com.offbynull.voip.kademlia.model.Node;
 import com.offbynull.voip.kademlia.model.Router;
+import static java.lang.Math.round;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,10 +40,12 @@ final class FindSubcoroutine implements Subcoroutine<List<Node>> {
     private final Address logAddress;
     private final AddressTransformer addressTransformer;
     private final IdGenerator idGenerator;
-    private final Router router;
     
+    private final Router router;
+    private final Id baseId;
     private final Id findId;
     private final int maxResults;
+    private final int concurrentRequests = 3;
     
     public FindSubcoroutine(Address subAddress, State state, Id findId, int maxResults) {
         Validate.notNull(subAddress);
@@ -50,8 +59,9 @@ final class FindSubcoroutine implements Subcoroutine<List<Node>> {
         logAddress = state.getLogAddress();
         addressTransformer = state.getAddressTransformer();
         idGenerator = state.getIdGenerator();
-        router = state.getRouter();
         
+        router = state.getRouter();
+        this.baseId = state.getBaseId();
         this.findId = findId;
         this.maxResults = maxResults;
     }
@@ -64,88 +74,72 @@ final class FindSubcoroutine implements Subcoroutine<List<Node>> {
     @Override
     public List<Node> run(Continuation cnt) throws Exception {
         Context ctx = (Context) cnt.getContext();
-        
-        IdClosenessComparator idClosenessComparator = new IdClosenessComparator(findId);
+
+        // Set up subcoroutine router
+        Address routerAddress = subAddress.appendSuffix("finderreq" + idGenerator.generate());
+        SubcoroutineRouter msgRouter = new SubcoroutineRouter(routerAddress, ctx);
+        Controller msgRouterController = msgRouter.getController();
         
         // Get initial set of nodes to query from routing table
-        List<Node> queryNodes = router.find(findId, maxResults);
-        ctx.addOutgoingMessage(subAddress, logAddress, info("Initial route table entries closest to {}: {}", findId, queryNodes));
+        List<Node> startNodes = router.find(findId, maxResults);
+        ctx.addOutgoingMessage(subAddress, logAddress, info("Initial route table entries closest to {}: {}", findId, startNodes));
+        
+        // Create sorted set of nodes to contact
+        IdClosenessComparator idClosenessComparator = new IdClosenessComparator(findId);
+        TreeSet<Node> contactSet = new TreeSet<>((x, y) -> idClosenessComparator.compare(x.getId(), y.getId()));
+        contactSet.addAll(startNodes);
+
+        // Create a sorted set of nodes to retain closest nodes in
+        TreeSet<Node> closestSet = new TreeSet<>((x, y) -> idClosenessComparator.compare(x.getId(), y.getId()));
         
         while (true) {
-            // If you've found the ID you're looking for, return immediately
-            Node closestQueriedNode = queryNodes.get(0);
-            if (closestQueriedNode.getId().equals(findId)) {
-                return queryNodes;
-            }
-            
-            
-            // Build multirequest to nodes in queryNodes
-            MultiRequestSubcoroutine.Builder<FindResponse> multiReqSubBuilder = new MultiRequestSubcoroutine.Builder<>();
-            
-            Map<String, Node> uniqueSuffixToQueryNode = new HashMap<>();
-            for (Node queryNode : queryNodes) {
-                String link = queryNode.getLink();
-                Address address = addressTransformer.linkIdToRemoteAddress(link);
+            // If there's room left to query more contacts that are closer to findId, do so... 
+            while (msgRouterController.size() < maxResults && !contactSet.isEmpty()) {
+                // Get next farthest away node to contact
+                Node contactNode = contactSet.pollLast();
                 
-                String uniqueRequestSourceSuffix = idGenerator.generate();
-                uniqueSuffixToQueryNode.put(uniqueRequestSourceSuffix, queryNode);
-                
-                multiReqSubBuilder.addDestinationAddress(uniqueRequestSourceSuffix, address);
-            }
-            
-            MultiRequestSubcoroutine<FindResponse> multiReqSub = multiReqSubBuilder
-                    .attemptInterval(Duration.ofMillis(2L))
-                    .maxAttempts(5)
-                    .addExpectedResponseType(FindResponse.class)
-                    .sourceAddress(subAddress.appendSuffix(idGenerator.generate()))
-                    .timerAddress(timerAddress)
-                    .build();
-            
-            
-            // Execute multirequest
-            List<Response<FindResponse>> responses = multiReqSub.run(cnt);
-            
-            
-            // Process responses from multirequest
-            Set<Node> nonResponsiveNodes = new HashSet<>(uniqueSuffixToQueryNode.values());
-            TreeSet<Node> sortedResponses = new TreeSet<>((x, y) -> idClosenessComparator.compare(x.getId(), y.getId()));
-            for (Response<FindResponse> response : responses) {
-                // Remove nodes that responded from nonResponsiveNodes
-                String reqSuffix = response.getUniqueSourceAddressSuffix();
-                Node queriedNode = uniqueSuffixToQueryNode.get(reqSuffix);
-                nonResponsiveNodes.remove(queriedNode);
-                
-                // Get response
-                FindResponse findResponse = response.getResponse();
-                Node[] nodes = findResponse.getNodes();
-                
-                // Touch responding node
-                router.touch(ctx.getTime(), queriedNode);
-                
-                // Add node results to sorted set
-                sortedResponses.addAll(Arrays.asList(nodes));
-                
-                // Make sure sortedNodeSet doesn't exceed past maxResults -- remove the ones that are farthest away
-                while (sortedResponses.size() > maxResults) {
-                    sortedResponses.pollLast();
+                // If we already have maxResult closer nodes to findId, skip this node
+                closestSet.add(contactNode);
+                if (closestSet.size() > maxResults) {
+                    Node removedNode = closestSet.pollLast();
+                    if (removedNode == contactNode) {
+                        continue;
+                    }
                 }
-            }
-            
-            
-            // Lock nodes that didn't respond
-            for (Node node : nonResponsiveNodes) {
                 
+                // Initialize query
+                RequestSubcoroutine<FindResponse> reqSubcoroutine = new RequestSubcoroutine.Builder<FindResponse>()
+                        .sourceAddress(routerAddress, idGenerator)
+                        .timerAddress(timerAddress)
+                        .request(new FindRequest(findId, maxResults))
+                        .addExpectedResponseType(FindResponse.class)
+                        .attemptInterval(Duration.ofSeconds(10L))
+                        .maxAttempts(1)
+                        .throwExceptionIfNoResponse(false)
+                        .build();
+                
+                // Add query to router
+                msgRouterController.add(reqSubcoroutine, AddBehaviour.ADD_PRIME_NO_FINISH);
             }
             
             
-            // Get only the nodes that are closer than the closest node you queried, if there are non, then return the set of nodes you
-            // queried
-            SortedSet<Node> closerThanQueriedNodes = sortedResponses.headSet(closestQueriedNode, false);
-            if (closerThanQueriedNodes.isEmpty()) {
-                return queryNodes;
-            }
+            // Forward the current message to the router
+            ForwardResult res = msgRouter.forward();
             
-            queryNodes = new ArrayList<>(closerThanQueriedNodes);
+            // If a request completed
+            if (res.isCompleted()) {
+                // Get response
+                FindResponse findResponse = (FindResponse) res.getResult();
+                
+                // Check if failure
+                if (findResponse == null) {
+                    DO SOMETIHNG HERE;
+                }
+                
+                // Add node results to contacts
+                Node[] nodes = findResponse.getNodes();
+                contactSet.addAll(Arrays.asList(nodes));
+            }
         }
     }
     
